a0be8f4b7e5680d65bbbcc21f531fa8cf80725ee
Preston Darrough <techdarrough@gmail.com>
2022-01-20 16:36:33 -0800

    functions created

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
-const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+// const character = newImage('assets/green-character/static.gif')
+// move(character).to(100, 250)
 
-//
+//moveCharacter
+
+const character = newImage('assets/green-character/static.gif')
+let direction = null;
+let x = 100;
+let y = 250;
+
+setInterval(function(){ 
+    if(direction === 'west'){
+        x = x - 1
+    }
+    if(direction === 'north'){
+        y = y + 1
+    }
+    if(direction === 'east'){
+        x = x + 1
+    }
+    if(direction === 'south'){
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}, 1)                                 
+
+move(character).to(100, 250)
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
