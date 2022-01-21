09ca88db60449593827144fd3f721a5739c4f63e
Preston Darrough <techdarrough@gmail.com>
2022-01-20 17:06:30 -0800

    final commit no bonus

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
-// const character = newImage('assets/green-character/static.gif')
-// move(character).to(100, 250)
-
-//moveCharacter
-
 const character = newImage('assets/green-character/static.gif')
-let direction = null;
-let x = 100;
-let y = 250;
 
-setInterval(function(){ 
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = `assets/green-character/static.gif`
+    }
     if(direction === 'west'){
-        x = x - 1
+        character.src = `assets/green-character/west.gif`
     }
     if(direction === 'north'){
-        y = y + 1
+        character.src = `assets/green-character/north.gif`
     }
     if(direction === 'east'){
-        x = x + 1
+        character.src = `assets/green-character/east.gif`
     }
     if(direction === 'south'){
-        y = y - 1
+        character.src = `assets/green-character/south.gif`
     }
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
-}, 1)                                 
+}
 
-move(character).to(100, 250)
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+
+
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
