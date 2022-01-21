cc308f6666897310f0df68c4912f898a38de39ad
Preston Darrough <techdarrough@gmail.com>
2022-01-20 16:00:41 -0800

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ move(inventory).to(0, 0)
 const character = newImage('assets/green-character/static.gif')
 move(character).to(100, 250)
 
-
+//
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
